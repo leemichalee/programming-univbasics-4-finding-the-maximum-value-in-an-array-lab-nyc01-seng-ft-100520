7f73debb30b400fc182ca3da36aa17e977bbb4fc
Lee Michaeli <leeisr@gmail.com>
2020-09-22 23:21:28 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,6 +1,6 @@
 def find_max_value(array)
   counter = 0
-  max_value
+  max_value = nil
   while counter < array.length do
     if array[counter] > array[counter - 1]
       max_value = array[counter]
